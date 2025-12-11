aa7aa0876a51e5bb080789fe6f852186dc561034
Murali <muralisurya2004@gmail.com>
2025-12-11 23:05:06 +0530

    Create orderby.java

---
+++ orderby.java
@@ -0,0 +1,44 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class sortdata 
+{
+public static void main(String [] args)
+{
+	try
+	{
+
+Class.forName("com.mysql.cj.jdbc.Driver");
+String dburl = ("jdbc:mysql://localhost:3306/dmart?user=root&password=Murali@6492");
+Connection con = DriverManager.getConnection(dburl);
+if(con != null)
+{
+System.out.println("connection successfull");	
+}
+else
+{
+System.out.println("connection unsuccessfull");	
+}
+
+String query = "SELECT NAME FROM STORE ORDER BY PRICE";
+Statement st = 	con.createStatement();
+ResultSet rs = st.executeQuery(query);
+while(rs.next())
+{
+	
+String name = rs.getString(1);
+System.out.println(name);
+}
+
+}
+	catch(ClassNotFoundException | SQLException e)
+	{
+		e.printStackTrace();
+	}
+}
+}
