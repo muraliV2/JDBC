c9f9709b6a40e302c39c8952a0ceb915b5159951
Murali <muralisurya2004@gmail.com>
2026-05-16 23:20:17 +0530

    Update executebatch.java

--- executebatch.java
+++ executebatch.java
@@ -17,9 +17,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import com.mysql.cj.protocol.Resultset;
-
 public class batch 
 {
 public static void main(String [] args)
