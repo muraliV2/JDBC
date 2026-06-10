b048b300ec9cf5673b85c1433805fa2f5bccd3e6
Murali <muralisurya2004@gmail.com>
2026-06-10 22:35:45 +0530

    Update updateimage.java

--- updateimage.java
+++ updateimage.java
@@ -6,7 +6,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 public class updateimage_audio 
 {
 public static void main(String [] args)
