33edc3634f04537fc3bdcde00ed7e5ca9555fb4d
Murali <muralisurya2004@gmail.com>
2026-06-18 20:37:29 +0530

    Update orderby1.java

--- orderby1.java
+++ orderby1.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 public class sortdata 
 {
 public static void main(String [] args)
